b6b83808ee7a55ae004fc9ebfde6208aa018035d
madocelot <46399117+madocelot@users.noreply.github.com>
2019-01-09 06:08:19 +0300

    Update README.m

--- README.m
+++ README.m
@@ -1,6 +1,2 @@
 # Django-plt
 This project usues matplotlib to plot graphs in Django framework. It uses no JS or front end frameworks. Postgre is used for DB.
-
-Example of a chart built with this tool
-
-![chart](https://raw.githubusercontent.com/madocelot/Django-plt/master/test.png)
