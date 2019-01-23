c1bb26a64233acfd0ab1417b79d7e549ca6969e3
madocelot <46399117+madocelot@users.noreply.github.com>
2019-01-23 15:41:13 +0300

    Update README.m

--- README.m
+++ README.m
@@ -1,6 +1,12 @@
 # Django-plt
 This project usues matplotlib to plot graphs in Django framework. It uses no JS or front end frameworks. Postgre is used for DB.
 
+django REST, Yarn, React, react-chartjs-2
 
 load DB data with:
 python manage.py loaddata *file*
+
+DB: user django_client, password = 'password'
+SU: 'admin', password = 'admin'
+
+
